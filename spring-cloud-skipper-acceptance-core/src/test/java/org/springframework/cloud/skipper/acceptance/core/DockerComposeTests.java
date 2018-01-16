decb95dee52ed43d564f60bcce28893b85c1dcb4
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-01-16 13:30:13 +0000

    stash

--- spring-cloud-skipper-acceptance-core/src/test/java/org/springframework/cloud/skipper/acceptance/core/DockerComposeTests.java
+++ spring-cloud-skipper-acceptance-core/src/test/java/org/springframework/cloud/skipper/acceptance/core/DockerComposeTests.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class DockerComposeTests  {
 
 	@DockerCompose(locations = {"docker-compose-3.yml", "docker-compose-4.yml"})
-	@Test
+	//@Test
 	public void testCompose1(DockerComposeInfo dockerComposeInfo) {
 		System.out.println("hello1 " + dockerComposeInfo);
 	}
 
 	@DockerCompose(locations = {"docker-compose-5.yml", "docker-compose-6.yml"})
-	@Test
+	//@Test
 	public void testCompose2(DockerComposeInfo dockerComposeInfo) {
 		System.out.println("hello2 " + dockerComposeInfo);
 	}
