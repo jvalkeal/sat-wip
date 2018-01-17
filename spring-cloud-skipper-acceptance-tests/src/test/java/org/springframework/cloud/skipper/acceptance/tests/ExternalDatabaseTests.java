85dcc6fbc1354b5e9396fdbadec29d005540f3e1
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-01-17 07:32:04 +0000

    stash

--- spring-cloud-skipper-acceptance-tests/src/test/java/org/springframework/cloud/skipper/acceptance/tests/ExternalDatabaseTests.java
+++ spring-cloud-skipper-acceptance-tests/src/test/java/org/springframework/cloud/skipper/acceptance/tests/ExternalDatabaseTests.java
@@ -1,7 +1,5 @@
 package org.springframework.cloud.skipper.acceptance.tests;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +7,6 @@ import org.springframework.cloud.skipper.acceptance.core.DockerCompose;
 import org.springframework.cloud.skipper.acceptance.core.DockerComposeInfo;
 import org.springframework.web.client.RestTemplate;
 
-import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.DockerPort;
 
 import static com.jayway.awaitility.Awaitility.with;
@@ -19,43 +16,27 @@ public class ExternalDatabaseTests {
 	@Test
 	@DockerCompose(locations = { "src/test/resources/skipper-postgres.yml" }, services = { "postgres", "skipper" })
 	public void testSkipperWithPostgres(DockerComposeInfo dockerComposeInfo) throws Exception {
-		DockerComposeRule docker = dockerComposeInfo.getRule();
-		DockerPort container1 = docker.containers().container("postgres").port(5432);	
-		assertThat(container1).isNotNull();
-		DockerPort container2 = docker.containers().container("skipper").port(7577);	
-		assertThat(container2).isNotNull();
-				
-		String url = "http://" + container2.getIp() + ":" + container2.getExternalPort() + "/api/about";
-		RestTemplate template = new RestTemplate();
-		
-		with()
-			.pollInterval(1, TimeUnit.SECONDS)
-			.and()
-		.await()
-			.ignoreExceptions()
-			.atMost(20, TimeUnit.SECONDS)
-			.until(() -> template.getForObject(url, String.class).contains("Spring Cloud Skipper Server"));
-		
+		DockerPort port = dockerComposeInfo.getRule().containers().container("skipper").port(7577);
+		String url = "http://" + port.getIp() + ":" + port.getExternalPort() + "/api/about";
+		assertServerRunning(url);
 	}
 
 	@Test
 	@DockerCompose(locations = { "src/test/resources/skipper-mysql.yml" }, services = { "mysql", "skipper" })
 	public void testSkipperWithMysql(DockerComposeInfo dockerComposeInfo) throws Exception {
-		DockerComposeRule docker = dockerComposeInfo.getRule();
-		DockerPort container1 = docker.containers().container("mysql").port(3306);	
-		assertThat(container1).isNotNull();		
-		DockerPort container2 = docker.containers().container("skipper").port(7577);	
-		assertThat(container2).isNotNull();
-
-		String url = "http://" + container2.getIp() + ":" + container2.getExternalPort() + "/api/about";
+		DockerPort port = dockerComposeInfo.getRule().containers().container("skipper").port(7577);
+		String url = "http://" + port.getIp() + ":" + port.getExternalPort() + "/api/about";
+		assertServerRunning(url);
+	}
+	
+	private void assertServerRunning(String url) {
 		RestTemplate template = new RestTemplate();
-		
 		with()
 			.pollInterval(1, TimeUnit.SECONDS)
 			.and()
-		.await()
-			.ignoreExceptions()
-			.atMost(20, TimeUnit.SECONDS)
-			.until(() -> template.getForObject(url, String.class).contains("Spring Cloud Skipper Server"));
-	}	
+			.await()
+				.ignoreExceptions()
+				.atMost(20, TimeUnit.SECONDS)
+				.until(() -> template.getForObject(url, String.class).contains("Spring Cloud Skipper Server"));
+	}
 }
