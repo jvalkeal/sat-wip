e6b93b499eec0702d03e61b08805a309319156a2
jvalkeal <janne.valkealahti@gmail.com>
2018-01-31 17:55:42 +0200

    tweak registry auth

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,6 @@ source $(dirname $0)/common.sh
 
 pushd git-repo > /dev/null
 echo $ARTIFACTORY_USERNAME
+echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
 ./gradlew clean build --refresh-dependencies
 popd > /dev/null
