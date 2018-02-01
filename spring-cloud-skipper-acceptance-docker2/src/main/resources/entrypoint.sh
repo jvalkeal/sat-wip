82330947a149ec06f369c6a1bddbadabca0aae0c
jvalkeal <janne.valkealahti@gmail.com>
2018-02-01 12:10:37 +0200

    shuffle stuff around and do some renames

--- spring-cloud-skipper-acceptance-docker2/src/main/resources/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 60
-exec java -Doracle.jdbc.timezoneAsRegion=false -Djava.security.egd=file:/dev/./urandom -jar /app.jar --spring.datasource.url="$SPRING_DATASOURCE_URL" --spring.datasource.username="$SPRING_DATASOURCE_USERNAME" --spring.datasource.password="$SPRING_DATASOURCE_PASSWORD" --spring.datasource.driver-class-name="$SPRING_DATASOURCE_DRIVER"
-
