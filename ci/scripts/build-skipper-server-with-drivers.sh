3bef572ff8a9ae25f1403bc9da86116102d81b67
jvalkeal <janne.valkealahti@gmail.com>
2018-02-01 15:50:30 +0200

    add logging

--- ci/scripts/build-skipper-server-with-drivers.sh
+++ ci/scripts/build-skipper-server-with-drivers.sh
@@ -4,11 +4,17 @@ set -e
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 echo distribution-repository ${repository}
-echo ls1
+echo xxx1
 ls
 
 pushd git-repo > /dev/null
+echo xxx2
+pwd
+echo xxx3
+ls
 cd custom-apps/skipper-server-with-drivers && ./gradlew clean build install -Dmaven.repo.local=${repository}
-echo ls2
+echo xxx4
+find ${repository}
+echo xxx5
 ls
 popd > /dev/null
