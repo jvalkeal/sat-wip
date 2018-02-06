8e9ca61db84346051d830ec1e6563c5a2204987f
jvalkeal <janne.valkealahti@gmail.com>
2018-02-06 14:14:16 +0200

    updates

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,8 +5,9 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-echo $ARTIFACTORY_USERNAME $BUILD_NAME
+echo $ARTIFACTORY_USERNAME
+echo $BUILD_NUMBER
 echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
 ./gradlew clean build
-tar zcf ${repository}/spring-cloud-skipper-acceptance-tests.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
+tar zcf ${repository}/spring-cloud-skipper-acceptance-tests-${BUILD_NUMBER}.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
 popd > /dev/null
