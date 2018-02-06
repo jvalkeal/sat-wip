668bf7110523d780c30d3014b7d0d4e9d7236d52
jvalkeal <janne.valkealahti@gmail.com>
2018-02-06 13:42:37 +0200

    test updates

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -2,9 +2,11 @@
 set -e
 
 source $(dirname $0)/common.sh
+repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
 echo $ARTIFACTORY_USERNAME
 echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
-./gradlew clean build --refresh-dependencies
+./gradlew clean build
+tar zcf ${repository}/spring-cloud-skipper-acceptance-tests.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
 popd > /dev/null
