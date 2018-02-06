f830717d5dba061a6453463067e84698a03faa2b
jvalkeal <janne.valkealahti@gmail.com>
2018-02-06 14:50:36 +0200

    change pipeline

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -3,11 +3,10 @@ set -e
 
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
+buildversion=`date '+%Y-%m-%d-%H-%M-%S'`
 
 pushd git-repo > /dev/null
-echo $ARTIFACTORY_USERNAME
-echo $BUILD_NUMBER
 echo $ARTIFACTORY_PASSWORD | docker login -u $ARTIFACTORY_USERNAME --password-stdin springsource-docker-private-local.jfrog.io
 ./gradlew clean build
-tar zcf ${repository}/spring-cloud-skipper-acceptance-tests-${BUILD_NUMBER}.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
+tar zcf ${repository}/spring-cloud-skipper-acceptance-tests-${buildversion}.tar.gz spring-cloud-skipper-acceptance-tests/build/test-docker-logs
 popd > /dev/null
